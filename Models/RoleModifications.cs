dbe25e52800d85d4c154948c84b85ea23c8c4a28
Jan Fiser <fishus@seznam.cz>
2024-08-15 04:05:49 +0200

    Edu connect 1.verze

---
+++ Models/RoleModifications.cs
@@ -0,0 +1,8 @@
+﻿namespace SchoolWebApp6_24.Models {
+    public class RoleModifications {
+        public string RoleName { get; set; }    
+        public string RoleId { get; set; }
+        public string[] AddIds { get; set; }
+        public string[] DeleteIds { get; set; }
+    }
+}
